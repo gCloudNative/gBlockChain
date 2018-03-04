59df03dbe1ce64c7aa6e5cc3afc817fabcf5f654
杨森 <yangsen1@niwodai.net>
2018-03-04 22:54:21 +0800

    os.getenviron.get for setting

--- gBlockChain/config.py
+++ gBlockChain/config.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
-DEBUG=False
-
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG=os.environ.get('DEBUG') or False
+SECRET_KEY = os.environ.get('SECRET_KEY') or ')=+A54#:aA5TFFND5W\t}!"W~E(k07*P'
+
 
 APP_NAME = 'gBlockChain'
 APP_LOG = '/tmp/gBlockChain.log'
 
-SECRET_KEY = ')=+A54#:aA5TFFND5W\t}!"W~E(k07*P'
 
 
