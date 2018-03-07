f103b5db425474530e9e4a3e322fd38531c83830
杨森 <yangsen1@niwodai.net>
2018-03-07 16:36:15 +0800

    add static/js

---
+++ gBlockChain/static/js/pages/datatables_extension_fixed_columns.js
@@ -0,0 +1,122 @@
+/* ------------------------------------------------------------------------------
+*
+*  # Fixed Columns extension for Datatables
+*
+*  Specific JS code additions for datatable_extension_fixed_columns.html page
+*
+*  Version: 1.0
+*  Latest update: Aug 1, 2015
+*
+* ---------------------------------------------------------------------------- */
+
+$(function() {
+
+    // Table setup
+    // ------------------------------
+
+    // Setting datatable defaults
+    $.extend( $.fn.dataTable.defaults, {
+        // columnDefs: [{
+        //     orderable: false,
+        //     width: '100px',
+        //     targets: [ 5 ]
+        // }],
+        // dom: '<"datatable-header"fl><"datatable-scroll datatable-scroll-wrap"t><"datatable-footer"ip>',
+        language: {
+            search: '<span>Filter:</span> _INPUT_',
+            searchPlaceholder: 'Type to filter...',
+            lengthMenu: '<span>Show:</span> _MENU_',
+            paginate: { 'first': 'First', 'last': 'Last', 'next': '&rarr;', 'previous': '&larr;' }
+        }
+    });
+
+
+    // Left fixed column example
+    var table = $('.datatable-fixed-left').DataTable({
+        columnDefs: [
+            {
+                orderable: false,
+                targets: [5]
+            },
+            {
+                width: "200px",
+                targets: [0]
+            },
+            {
+                width: "300px",
+                targets: [1]
+            },
+            {
+                width: "200px",
+                targets: [5, 6]
+            },
+            {
+                width: "100px",
+                targets: [4]
+            }
+        ],
+        scrollX: true,
+        // scrollY: '350px',
+        scrollCollapse: true,
+        fixedColumns: true
+    });
+
+    // var table = $('.datatable-fixed-complex').DataTable({
+    //     autoWidth: false,
+    //     columnDefs: [
+    //         {
+    //             orderable: false,
+    //             targets: [5]
+    //         },
+    //         {
+    //             width: "250px",
+    //             targets: [0]
+    //         },
+    //         {
+    //             width: "250px",
+    //             targets: [1]
+    //         },
+    //         {
+    //             width: "200px",
+    //             targets: [5, 6]
+    //         },
+    //         {
+    //             width: "100px",
+    //             targets: [4]
+    //         }
+    //     ],
+    //     scrollX: true,
+    //     scrollY: '350px',
+    //     scrollCollapse: true,
+    //     fixedColumns: true
+    // });
+
+    // Adjust columns on window resize
+    setTimeout(function() {
+        $(window).on('resize', function () {
+            table.columns.adjust();
+        });
+    }, 100);
+
+
+
+    // External table additions
+    // ------------------------------
+
+    // Enable Select2 select for the length option
+    $('.dataTables_length select').select2({
+        minimumResultsForSearch: Infinity,
+        width: 'auto'
+    });
+
+
+
+
+
+
+
+
+
+
+    
+});
