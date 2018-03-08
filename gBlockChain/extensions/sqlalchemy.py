21ed38c3401abf8d1ce46fa725eecbec6c130d6e
杨森 <yangsen1@niwodai.net>
2018-03-09 01:11:13 +0800

    add "use_native_unicode=utf8"

--- gBlockChain/extensions/sqlalchemy.py
+++ gBlockChain/extensions/sqlalchemy.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from flask_sqlalchemy import SQLAlchemy
-# from gBlockChain.models import *
 
 class NewSQLAlchemy(SQLAlchemy):
-    def init_app(self, app):
+    def init_app(self, app, use_native_unicode='utf8' ):
         super(NewSQLAlchemy, self).init_app(app)
         self.create_all()
 
